cf4bf33ed913364a17c718826c48d39c0e481bf2
Jorge Alberto Díaz Orozco <diazorozcoj@gmail.com>
2017-07-21 15:00:31 -0400

    - back from that

--- pingapp/views.py
+++ pingapp/views.py
@@ -6,9 +6,8 @@ from rest_framework.response import Response
 @api_view(['GET', 'POST'])
 def ping(request):
     ip = request.META.get('HTTP_X_FORWARDED_FOR', None)
-    try:
-        data = requests.get('http://ip-api.com/json/' + ip).json()
-        if data.get('status') != 'fail':
-            return Response({'success': True, 'ip': ip, 'geodata': data})
-    finally:
+    data = requests.get('http://ip-api.com/json/' + ip).json()
+    if data.get('status') != 'fail':
+        return Response({'success': True, 'ip': ip, 'geodata': data})
+    else:
         return Response({'success': True, 'ip': ip})
