6771e8035ac2972238d1bd51959aae76f26611f8
Jorge Alberto Díaz Orozco <diazorozcoj@gmail.com>
2017-07-21 14:45:57 -0400

    - handle possible requests error

--- pingapp/views.py
+++ pingapp/views.py
@@ -6,8 +6,9 @@ from rest_framework.response import Response
 @api_view(['GET', 'POST'])
 def ping(request):
     ip = request.META.get('HTTP_X_FORWARDED_FOR', None)
-    data = requests.get('http://ip-api.com/json/' + ip).json()
-    if data.get('status') != 'fail':
-        return Response({'success': True, 'ip': ip, 'geodata': data})
-    else:
+    try:
+        data = requests.get('http://ip-api.com/json/' + ip).json()
+        if data.get('status') != 'fail':
+            return Response({'success': True, 'ip': ip, 'geodata': data})
+    finally:
         return Response({'success': True, 'ip': ip})
